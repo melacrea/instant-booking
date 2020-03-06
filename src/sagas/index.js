5ad0b38dee9d6034298ca1c5f90e62e670e518b2
Melanie Lecomte <melacrea@gmail.com>
2020-03-06 21:52:04 +0100

    store jwt token into localstorage

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,13 @@
 import { put, takeLatest, all } from 'redux-saga/effects';
+const url =  process.env.REACT_APP_API_ENDPOINT
+
 function* fetchRoomInfos() {
-  const json = yield fetch('http://localhost:4000/login')
-        .then(response => response.json(), );
+  const json = yield fetch(`${url}/login`)
+        .then(response => response.json());
+
+  if(json.success)
+    localStorage.setItem('jwt_token', json.data.token)
+  
   yield put({ type: "TOKEN_RECEIVED", payload: json.success});
 }
 function* actionWatcher() {
